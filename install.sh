6a2d1b1b4720dacf130bd6a4ae82b1fa982e5ea8
Coordinate-Cat <tetrapasta02@gmail.com>
2022-01-11 12:21:06 +0900

    ✨ :update, install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,55 @@
-#!/bin/sh
+#!/bin/zsh
 
-dirs=$(find "$PWD" -depth 1 -type d -not -name '.*' | sort -n)
-for dir in $dirs;
-do
-  echo 📁 "$dir"
-  sh "$dir"/install.sh
-done
+DOT_DIR="$HOME/dotfiles"
+
+has() {
+    type "$1" > /dev/null 2>&1
+}
+
+HELLO_ASCII='\e[32;5;7m
+                                                         
+  ██╗  ██╗ ██╗      ██████╗   ██████╗  █████╗  ████████╗ 
+  ██║  ██║ ██║     ██╔═══██╗ ██╔════╝ ██╔══██╗ ╚══██╔══╝ 
+  ███████║ ██║     ██║   ██║ ██║      ███████║    ██║    
+  ██╔══██║ ██║     ██║   ██║ ██║      ██╔══██║    ██║    
+  ██║  ██║ ██║ ▄█╗ ╚██████╔╝ ╚██████╗ ██║  ██║    ██║    
+  ╚═╝  ╚═╝ ╚═╝ ╚═╝  ╚═════╝   ╚═════╝ ╚═╝  ╚═╝    ╚═╝    
+                                                          
+\e[31m
+'
+
+if [ ! -d ${DOT_DIR} ]; then
+    if has "git"; then
+        git clone https://github.com/Coordinate-Cat/dotfiles.git ${DOT_DIR}
+    # If you don't have git, use curl or wget.
+    elif has "curl" || has "wget"; then
+        TARBALL="https://github.com/Coordinate-Cat/dotfiles/archive/master.tar.gz"
+        if has "curl"; then
+            curl -L ${TARBALL} -o master.tar.gz
+        else
+            wget ${TARBALL}
+        fi
+        tar -zxvf master.tar.gz
+        rm -f master.tar.gz
+        mv -f dotfiles-master "${DOT_DIR}"
+    else
+        echo "curl or wget or git required"
+        exit 1
+    fi
+
+    cd ${DOT_DIR}
+    for f in *;
+    do
+        [[ "$f" == ".git" ]] && continue
+        [[ "$f" == ".gitignore" ]] && continue
+        [[ "$f" == ".DS_Store" ]] && continue
+        [[ "$f" == "README.md" ]] && continue
+        [[ "$f" == "install.sh" ]] && continue
+
+        ln -snf $DOT_DIR/"$f" $HOME/".$f"
+        echo "Installed .$f"
+    done
+else
+    clear && echo "$HELLO_ASCII"
+    exit 1
+fi
