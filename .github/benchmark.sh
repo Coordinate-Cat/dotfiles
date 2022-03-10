2d417ccaef4869eebbd89338641b719ccc20f9e2
Coordinate-Cat <tetrapasta02@gmail.com>
2022-03-10 23:10:06 +0900

    🔁 refactor: fuck

--- .github/benchmark.sh
+++ .github/benchmark.sh
@@ -2,13 +2,9 @@
 # https://zenn.dev/odan/articles/17a86574b724c9
 set -eux
 
-rm -f benchmark-results/*
+gtime --format="%e" zsh -i -c exit 2> benchmark-results/zsh-install-time.txt
 
-gtime --format="%e" --output=benchmark-results/zsh-install-time.txt zsh -i -c exit
-
-for i in $(seq 1 10); do
-    gtime --format="%e" --output=benchmark-results/zsh-load-time-${i}.txt zsh -i -c exit;
-done
+{ for i in $(seq 1 10); do gtime --format="%e" zsh -i -c exit; done } 2> benchmark-results/zsh-load-time.txt
 
 ZSH_LOAD_TIME=$(cat benchmark-results/zsh-load-time.txt | awk '{ total += $1 } END { print total/NR }')
 ZSH_INSTALL_TIME=$(cat benchmark-results/zsh-install-time.txt)
